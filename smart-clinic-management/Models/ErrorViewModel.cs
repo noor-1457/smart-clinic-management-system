01553502f5b8e4190fd14d2ff7573b6b83290195
Noori <noorzafar882@gmail.com>
2025-12-09 17:22:57 +0500

    Project Initialized

---
+++ smart-clinic-management/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace smart_clinic_management.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
